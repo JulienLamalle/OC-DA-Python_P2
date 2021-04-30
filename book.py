c3cd982b2bd184486aa0b690eb550fa4012fcc58
JulienLamalle <julienlamalle63100@gmail.com>
2021-04-30 08:17:58 +0200

    using try/except for get request with requests module

--- book.py
+++ book.py
@@ -70,35 +70,46 @@ class Book:
       return
 
   def get_book_informations(self, url, category, base_url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    if response.status_code == 200:
-      book_informations = {
-        'universal_product_code': self.get_universal_product_code(soup),
-        'title': self.get_title(soup),
-        'price_including_tax': self.get_price_including_tax(soup),
-        'get_price_excluding_tax': self.get_price_excluding_tax(soup),
-        'number_available': self.get_number_available(soup),
-        'product_description': self.get_product_description(soup),
-        'category': self.get_category(soup),
-        'review_rating': self.get_review_rating(soup),
-        'image_url': self.get_image_url(soup, base_url),
-        'image_filename': slugify(self.get_title(soup))
-      }
-      self.save_book_informations_to_csv(book_informations, category)
-      self.save_book_image(soup, book_informations, category)
+    try:
+      response = requests.get(url)
+      soup = BeautifulSoup(response.content, 'html.parser')
+      if response.status_code == 200:
+        book_informations = {
+          'universal_product_code': self.get_universal_product_code(soup),
+          'title': self.get_title(soup),
+          'price_including_tax': self.get_price_including_tax(soup),
+          'get_price_excluding_tax': self.get_price_excluding_tax(soup),
+          'number_available': self.get_number_available(soup),
+          'product_description': self.get_product_description(soup),
+          'category': self.get_category(soup),
+          'review_rating': self.get_review_rating(soup),
+          'image_url': self.get_image_url(soup, base_url),
+          'image_filename': slugify(self.get_title(soup))
+        }
+        self.save_book_informations_to_csv(book_informations, category)
+        self.save_book_image(soup, book_informations, category)
+      elif not response.status_code // 100 == 2:
+        print(f'Error: Unexpected response {response}')
+    except request.exceptions.RequestException as error:
+      print(f'Error: {error}')
+  
 
   def save_book_image(self, soup, book_informations, category):
-    response = requests.get(book_informations['image_url'], stream=True)
-    if response.status_code == 200:
-      dir_path = f'data/{category}/images'
-      filename = book_informations['image_filename']
-      if not os.path.exists(dir_path):
-        os.makedirs(f'data/{category}/images')
-      file_path = os.path.join(dir_path, filename)
-      response.raw.decode_content = True
-      with open(f'{file_path}', 'wb') as file:
-        shutil.copyfileobj(response.raw, file)
+    try:
+      response = requests.get(book_informations['image_url'], stream=True)
+      if response.status_code == 200:
+        dir_path = f'data/{category}/images'
+        filename = book_informations['image_filename']
+        if not os.path.exists(dir_path):
+          os.makedirs(f'data/{category}/images')
+        file_path = os.path.join(dir_path, filename)
+        response.raw.decode_content = True
+        with open(f'{file_path}', 'wb') as file:
+          shutil.copyfileobj(response.raw, file)
+      elif not response.status_code // 100 == 2:
+        print(f'Error: Unexpected response {response}')
+    except request.exceptions.RequestException as error:
+      print(f'Error: {error}')
 
   def save_book_informations_to_csv(self, book_informations: dict, category):
     dir_path = f'data/{category}'
